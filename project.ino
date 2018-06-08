1b85692849fa6a566aa01d18d043c99d0b9b3fd4
Ted <teddychenster@gmail.com>
2018-06-08 13:50:18 +0800

    last
    0608  從mac拿新檔案前

--- project.ino
+++ project.ino
@@ -1,20 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-// Set these to run example.
-#define FIREBASE_HOST "light-test-f2c8e.firebaseio.com"
-#define FIREBASE_AUTH "pM6onwRR2jlWHn6zLUv9qqOS8eVNnkdblrdX9iOy"
+// Set parameters to connect to firebase
+#define FIREBASE_HOST "light-test-7d9fa.firebaseio.com"
+#define FIREBASE_AUTH "mhMewzfKimcL7TTpjoEFRKltxjDScIgkCiNrXNWz"
 #define WIFI_SSID "qwer"
 #define WIFI_PASSWORD "qqqqqqqq"
 
-int buttonPin = D6;
+int buttonPin = D6;     //must add D before pin number or else compile error
 
 
 void setup() {
   Serial.begin(9600);
 
   // connect to wifi.
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);       //WiFi is Defined in esp8266Wifi.h
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
@@ -27,36 +27,33 @@ void setup() {
   pinMode(buttonPin, INPUT); 
   pinMode(LED_BUILTIN, HIGH);
 
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);   // connect arduino to firebase database
 
 }
 
-int interval = 100;
-bool light ;
+// Global variables
+int interval = 100;           // initialize interval for flash
+bool light ;                  // whether the light should blink
 
 //Set Debounce
 //    To prevent elders from continuously pressing button
 //    Set time interval to 5 mins.
 //    Can be alterd
 
-
 unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 5000; //5 sec 3e5;   //5mins
+unsigned long debounceDelay = 5000;       // time until next time it can press
 int lastButtonState = LOW;
 bool canPress = true;
 int dbounce=0;  //debonce for internet
 
 void loop() {
   //get data from firebase
-//  Serial.println(dbounce);
   if (Firebase.getBool("alter") == true && dbounce >= 6 ) // in case the database hasn't change its state
   {
-//    Serial.println("changed");
     light = Firebase.getBool("light");
     interval = Firebase.getInt("flash");
     Firebase.setBool("alter", false);
     dbounce = 0;
-//    Serial.println(interval);
   }
 
   if (light == true) {
@@ -69,39 +66,32 @@ void loop() {
     digitalWrite(D0, LOW);
     digitalWrite(LED_BUILTIN, LOW);
   }
-//  Serial.println(light);
   dbounce++;
 
-  //send data to firebase;
-//  Serial.print("canPress:");
-//  Serial.println(canPress);
-//  Serial.print("State:");
-//  Serial.println(reading);
+  //send data to firebase while button pressed
  
   int reading = digitalRead(buttonPin);
-  Serial.print("Reading=");
-  Serial.println(reading);
+      Serial.print(reading);
   if (canPress && reading == HIGH) {
-    Serial.println("clicked");
     lastButtonState = reading;
     canPress = !canPress;
+//    Serial.println(canPress);
     lastDebounceTime = millis();
+    
     // do things
-    DynamicJsonBuffer jsonBuffer;
+    DynamicJsonBuffer jsonBuffer;                             // create json object to push data to firebase
     JsonObject& clickEvent = jsonBuffer.createObject();
-    clickEvent[".sv"] = "timestamp";
+    clickEvent[".sv"] = "timestamp";                          // save system time
     Firebase.push("clicked", clickEvent);
-  }else if(!canPress){
-      Serial.print("Current time");
-      Serial.println(millis());
-      Serial.print("Last Debounce Time:");
-      Serial.println(lastDebounceTime);
+    Serial.println("sended");
+    }else if(!canPress){                                      // if one is unable to press
+      if (millis() - lastDebounceTime >= debounceDelay) {     // if it is down to the reset time
+        canPress = !canPress;                                 // toggle the state of canPress
+//            Serial.println(canPress);
 
-  if (millis() - lastDebounceTime >= debounceDelay) {
-    Serial.println("click enabled");
-    canPress = !canPress;
-  } 
+      } 
   }
       
 
 }
+
